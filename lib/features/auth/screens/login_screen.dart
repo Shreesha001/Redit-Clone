06c96aa6a2ecb118ea27af3d68075b59cbffdf52
Shreesha Shetty <shettyshreesha552@gmail.com>
2025-04-04 18:19:04 +0530

    Create login screen ui

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/core/common/sign_in_button.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+          fit:
+              BoxFit
+                  .contain, //BoxFit.contain ensures the image doesn't get cropped or distorted.
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Skip",
+              style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 30),
+          const Text(
+            "Dive into anything",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(Constants.loginEmotePath, height: 400),
+          ),
+          const SizedBox(height: 20),
+          const SignInButton(),
+        ],
+      ),
+    );
   }
 }
